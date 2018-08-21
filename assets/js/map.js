3eaddf0007c7b23782fe80158341c781b76d30aa
Jorge Zetina <jorgezetina@gmail.com>
2018-08-21 15:44:58 -0500

    Versión 1 completa
    TODO: Agregar menú

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,9 +1,16 @@
 var map;
 function initMap() {
-  var crown = {lat: 18.002037, lng: -92.946855};
+  var carmen = {lat: 18.6525473, lng: -91.8149856};
+  var campeche = {lat: 19.855165, lng: -90.527795};
+  var coatza = {lat: 18.145147, lng: -94.4281916};
+  var juarez = {lat: 31.6987674, lng: -106.4112108};
+  var merida = {lat: 21.0175579, lng: -89.6062588};
+  var navojoa = {lat: 27.0843706, lng: -109.4560122};
+  var villahermosa = {lat: 18.0019428, lng: -92.9467854};
+  var crown = {lat: 24.6944808, lng: -101.6182094};
   map = new google.maps.Map(document.getElementById('map'), {
     center: crown,
-    zoom: 17,
+    zoom: 5.4,
     zoomControl: true,
     mapTypeControl: false,
     scaleControl: true,
@@ -183,5 +190,47 @@ function initMap() {
         ]
       }
     ]
+  });
+
+  var marker = new google.maps.Marker({
+    position: campeche,
+    map: map,
+    title: 'Crown City Casino Campeche'
+  });
+
+  var marker = new google.maps.Marker({
+    position: carmen,
+    map: map,
+    title: 'Crown City Casino Carmen'
+  });
+
+  var marker = new google.maps.Marker({
+    position: coatza,
+    map: map,
+    title: 'Crown City Casino Coatzacoalcos'
+  });
+
+  var marker = new google.maps.Marker({
+    position: juarez,
+    map: map,
+    title: 'Crown City Casino Juárez'
+  });
+
+  var marker = new google.maps.Marker({
+    position: merida,
+    map: map,
+    title: 'Crown City Casino Mérida'
+  });
+
+  var marker = new google.maps.Marker({
+    position: navojoa,
+    map: map,
+    title: 'Crown City Casino Navojoa'
+  });
+
+  var marker = new google.maps.Marker({
+    position: villahermosa,
+    map: map,
+    title: 'Crown City Casino Villahermosa'
   });
 }
